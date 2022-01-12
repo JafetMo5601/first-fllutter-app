3b79ed2fe931f684a3854bb84a2f0da28f96eb8b
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-01-11 21:27:48 -0600

    Add a navigator function to push the new pages.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:first_flutter_app/src/pages/list_page.dart';
+import 'package:first_flutter_app/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 
@@ -11,8 +13,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          child: Text('Open the app!'),
-          onPressed: () {},
+          child: Text('Initiate the app!'),
+          onPressed: () {
+            Navigator.pushNamed(context, 'list_page');
+          },
         ),
       ),
     );
