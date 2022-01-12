d301cb6bcc4ed676f2ffdc204fa5a059c39c5264
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-01-11 21:26:08 -0600

    Create generic pages for the list redirection.

---
+++ lib/src/pages/cards_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class CardsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards Page'),
+      ),
+    );
+  }
+}
