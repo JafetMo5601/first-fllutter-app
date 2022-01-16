85ccca7fb388f3bfaeea8aec99c3efd5a02d6a9d
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-01-15 21:28:38 -0600

    Add new routes to auth pages.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,9 @@ import 'package:first_flutter_app/src/pages/avatar_page.dart';
 import 'package:first_flutter_app/src/pages/cards_page.dart';
 import 'package:first_flutter_app/src/pages/home_page.dart';
 import 'package:first_flutter_app/src/pages/list_page.dart';
+import 'package:first_flutter_app/src/pages/auth/login_page.dart';
+import 'package:first_flutter_app/src/pages/auth/forgot_password.dart';
+import 'package:first_flutter_app/src/pages/auth/register_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -14,5 +17,8 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert_page': (BuildContext context) => AlertPage(),
     'avatar_page': (BuildContext context) => AvatarPage(),
     'cards_page': (BuildContext context) => CardsPage(),
+    'login_page': (BuildContext context) => LoginPage(),
+    'register_page': (BuildContext context) => RegisterPage(),
+    'forgot_password_page': (BuildContext context) => ForgotPasswordPage()
   };
 }
